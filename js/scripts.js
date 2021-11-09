c66e5011aff94aa26ba8183a9ee4cc066a5cc62b
cpinopacheco <cpinopacheco64@gmail.com>
2021-11-09 20:32:46 -0300

    add multi language functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,24 @@ const toggleIcon = document.getElementById("toggle-icon");
 const toggleText = document.getElementById("toggle-text");
 const toggleColors = document.getElementById("toggle-colors");
 const rootStyles = document.documentElement.style;
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`js/languages/${language}.json`);
+  const texts = await requestJson.json();
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+    console.log(section, value);
+    textToChange.innerHTML = texts[section][value];
+  }
+};
+
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
 
 toggleTheme.addEventListener("click", (e) => {
   document.body.classList.toggle("dark");
